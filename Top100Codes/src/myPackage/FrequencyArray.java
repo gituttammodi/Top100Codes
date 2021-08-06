8cff12612f62a6ad01657fd70847523c6ea592b7
Uttam Modi <uttam3179@gmail.com>
2021-08-06 23:40:19 +0530

    Frequency array file is commited

---
+++ Top100Codes/src/myPackage/FrequencyArray.java
@@ -0,0 +1,27 @@
+package myPackage;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class FrequencyArray {
+	h
+
+	public static void main(String[] args) {
+		int arr[]= {2,3,5,8,3,4,7,2,8,5,4};
+		frequencyArray(arr);
+
+	}
+
+	private static void frequencyArray(int[] arr) {
+	Arrays.sort(arr);
+	for (int i = 0; i < arr.length; i++) {
+		int cur=arr[i];
+		
+	
+		
+	}
+		
+		
+	}
+
+}
